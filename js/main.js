4754bcafb8f6ec9223c0039de670f222bdef5807
jensdeg <jensdegraaf88@gmail.com>
2025-09-01 21:55:50 +0200

    reset buttons + score saving

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
-const buttons = document.querySelectorAll(".Ball-button");
+const buttons = document.querySelectorAll(".Ball-button, .clear-button");
 
 function updateScore(button) {
     const scoreElement = button.closest(".Score-window").querySelector(".Score");
     const currentScore = parseInt(scoreElement.textContent);
+    if(button.textContent === "Reset") {
+        scoreElement.textContent = "0";
+        saveScores();
+        return;
+    }
     const pointsToAdd = parseInt(button.textContent);
     const newScore = currentScore + pointsToAdd;
+    if (newScore < 0) {
+        newScore = 0;
+    }
     scoreElement.textContent = newScore;
+    saveScores();
+}
+
+// score saving
+let player1Score = parseInt(localStorage.getItem("player1Score")) || 0;
+let player2Score = parseInt(localStorage.getItem("player2Score")) || 0;
+
+document.getElementById("Player1Score").textContent = player1Score;
+document.getElementById("Player2Score").textContent = player2Score;
+
+function saveScores() {
+    localStorage.setItem("player1Score", document.getElementById("Player1Score").textContent);
+    localStorage.setItem("player2Score", document.getElementById("Player2Score").textContent);
 }
 
 buttons.forEach(button => {
@@ -22,6 +43,7 @@ buttons.forEach(button => {
     button.addEventListener("mouseenter", () => {
         button.classList.add("active");
     });
+    
     // touchscreen
     button.addEventListener("touchstart", () => {
         button.classList.add("active");
